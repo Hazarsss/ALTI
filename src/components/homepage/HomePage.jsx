58d04ee28ed45ac3dc0ae3af4a17c320823d5cde
unknown <ibnuh.hazars@gmail.com>
2025-02-09 23:41:41 +0700

    deploy github

--- src/components/homepage/HomePage.jsx
+++ src/components/homepage/HomePage.jsx
@@ -4,11 +4,14 @@ import {
   Card,
   CardHeader,
   CardBody,
-  CardFooter,
   Typography,
-  Avatar,
-  Tooltip,
+  Button,
+  CardFooter,
 } from "@material-tailwind/react";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import { useState } from "react";
 
 const HomePage = () => {
   const events = [
@@ -16,7 +19,7 @@ const HomePage = () => {
       location: "Area Hutan, Kalisoro",
       date: "11 - 13 Desember 2025",
       regis: "Registration Open",
-      image: "siksorogo.jpg",
+      image: "lawu.jpg",
       iconNationalis: "indonesia.png",
       iconCalendar: "calendar.png",
       iconJarak: "jarak.png",
@@ -44,7 +47,7 @@ const HomePage = () => {
       location: "Kintamani Bali",
       date: "9 - 11 May 2025",
       regis: "Registration Closed",
-      image: "btr.jpg",
+      image: "bromo.jpg",
       iconNationalis: "indonesia.png",
       iconCalendar: "calendar.png",
       iconJarak: "jarak.png",
@@ -54,6 +57,37 @@ const HomePage = () => {
       icon30k: "30k.png",
     },
   ];
+
+  const sponsors = [
+    {
+      sponsornames: "HOKA TECTON X3",
+      sponsordescription:
+        "Ready to propel you through gnarly terrain, the Tecton X 3 adds winglets to its carbon fiber plates for extra stability in just the right spots, and two layers of PEBA in the midsole—our lightest",
+      sponsorimage: "tectonX3.png",
+    },
+    {
+      sponsornames: "COROS PACE 3",
+      sponsordescription:
+        "Cutting edge sports technology & training methods for athletes, explorers, and outdoor enthusiasts worldwide. Our motto is simple: #ExplorePerfection",
+      sponsorimage: "coros.png",
+    },
+    {
+      sponsornames: "MAURTEN",
+      sponsordescription:
+        "Hydrogel Sports Fuel – Carbohydrate-rich fuel for endurance sports, based on natural ingredients for energy, hydration and recovery.",
+      sponsorimage: "maurten.png",
+    },
+  ];
+
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 2,
+    slidesToScroll: 1,
+  };
+  const bromobackground = "bromobackground.png";
+  const [bgEventImage, setBgEventImage] = useState(bromobackground);
 
   return (
     <>
@@ -88,106 +122,183 @@ const HomePage = () => {
             </button>
           </div>
         </div>
-        <div className="event-labelcontainer">
-          <p className="text-2xl">UPCOMING</p>
-          <p className="font-bold text-8xl">BIG EVENT</p>
-        </div>
-        <div className="event-section gap-14">
-          {events.map((event, index) => (
-            <Card
-              key={index}
-              className="max-w-[28rem] h-full flex flex-col justify-between overflow-hidden"
-            >
-              <CardHeader
-                floated={false}
-                shadow={false}
-                color="transparent"
-                className="m-0 rounded-none"
+        <div
+          className="event-container transition-all duration-500 bg-cover bg-center h-[100vh]"
+          style={{
+            backgroundImage: bgEventImage
+              ? `url(${bgEventImage})`
+              : `url(${bromobackground})`,
+          }}
+        >
+          <div className="event-labelcontainer">
+            <p className="upcominglabel">UPCOMING</p>
+            <p className="eventlabel font-bold">EVENTS</p>
+          </div>
+          <div className="event-section gap-14">
+            {events.map((event, index) => (
+              <Card
+                key={index}
+                className="w-120 flex flex-col justify-between overflow-hidden transition-all duration-500"
+                onMouseEnter={() => setBgEventImage(event.image)} // Change background on hover
+                onMouseLeave={() => setBgEventImage("")} // Reset background when leaving
               >
-                <img
-                  src={event.image}
-                  alt={event.location}
-                  className="w-full h-48 object-cover"
-                />
-              </CardHeader>
-              <CardBody className="cardevent-body items-center grid grid-cols-7 gap-3">
-                <div className="flex justify-center">
-                  <img
-                    className="w-8"
-                    src={event.iconNationalis}
-                    alt="iconNationalis"
-                  />
-                </div>
-                <Typography
-                  className="col-start-2 col-end-8"
-                  variant="h10"
-                  color="blue-gray"
+                <CardHeader
+                  floated={false}
+                  shadow={false}
+                  color="transparent"
+                  className="m-0 rounded-none"
                 >
-                  {event.location}
-                </Typography>
-                <div className="flex justify-center">
                   <img
-                    className="w-8"
-                    src={event.iconCalendar}
-                    alt="iconCalendar"
+                    src={event.image}
+                    alt={event.location}
+                    className="w-full h-48 object-cover"
                   />
-                </div>
-                <Typography
-                  className="col-start-2 col-end-8"
-                  variant="h10"
-                  color="gray"
-                >
-                  {event.date}
-                </Typography>
-                <div className="flex justify-center">
-                  <img
-                    className="w-8"
-                    src={event.iconJarak}
-                    alt="iconJarak....."
-                  />
-                </div>
-                <Typography
-                  className="col-start-2 col-end-8"
-                  variant="h10"
-                  color="gray"
-                >
-                  <div className="flex">
-                    {[
-                      event.icon100k,
-                      event.icon60k,
-                      event.icon30k,
-                      event.icon15k,
-                      event.icon7k,
-                    ]
-                      .filter((icon) => icon) // Remove empty or undefined icons
-                      .slice(0, 5) // Show only the first 3 available icons
-                      .map((icon, index) => (
-                        <img
-                          key={index}
-                          className="w-16"
-                          src={icon}
-                          alt={`icon-${index}`}
-                        />
-                      ))}
+                </CardHeader>
+                <CardBody className="cardevent-body items-center grid grid-cols-7 gap-3">
+                  <div className="flex justify-center">
+                    <img
+                      className="w-8"
+                      src={event.iconNationalis}
+                      alt="iconNationalis"
+                    />
                   </div>
-                </Typography>
-                <div className="flex justify-center">
-                  <img
-                    className="w-3"
-                    src={event.iconRegistrasi}
-                    alt="iconCalendar"
-                  />
-                </div>
-                <Typography
-                  className="col-start-2 col-end-8"
-                  variant="h10"
-                  color="gray"
-                >
-                  {event.regis}
-                </Typography>
-              </CardBody>
-            </Card>
-          ))}
+                  <Typography
+                    className="col-start-2 col-end-8"
+                    variant="h10"
+                    color="blue-gray"
+                  >
+                    {event.location}
+                  </Typography>
+                  <div className="flex justify-center">
+                    <img
+                      className="w-8"
+                      src={event.iconCalendar}
+                      alt="iconCalendar"
+                    />
+                  </div>
+                  <Typography
+                    className="col-start-2 col-end-8"
+                    variant="h10"
+                    color="gray"
+                  >
+                    {event.date}
+                  </Typography>
+                  <div className="flex justify-center">
+                    <img
+                      className="w-8"
+                      src={event.iconJarak}
+                      alt="iconJarak....."
+                    />
+                  </div>
+                  <Typography
+                    className="col-start-2 col-end-8"
+                    variant="h10"
+                    color="gray"
+                  >
+                    <div className="flex">
+                      {[
+                        event.icon100k,
+                        event.icon60k,
+                        event.icon30k,
+                        event.icon15k,
+                        event.icon7k,
+                      ]
+                        .filter((icon) => icon) // Remove empty or undefined icons
+                        .slice(0, 5) // Show only the first 3 available icons
+                        .map((icon, index) => (
+                          <img
+                            key={index}
+                            className="w-16"
+                            src={icon}
+                            alt={`icon-${index}`}
+                          />
+                        ))}
+                    </div>
+                  </Typography>
+                  <div className="flex justify-center">
+                    <img
+                      className="w-3"
+                      src={event.iconRegistrasi}
+                      alt="iconCalendar"
+                    />
+                  </div>
+                  <Typography
+                    className="col-start-2 col-end-8"
+                    variant="h10"
+                    color="gray"
+                  >
+                    {event.regis}
+                  </Typography>
+                  <button className="btnDiscover col-start-2 col-end-7 font-bold text-white">
+                    DISCOVER
+                  </button>
+                </CardBody>
+              </Card>
+            ))}
+          </div>
+        </div>
+        <div className="mainsponsor-section flex justify-center items-center">
+          <Slider {...settings} className="w-full max-w-[100rem]">
+            {sponsors.map((event, index) => (
+              <div key={index} className="flex justify-center items-center p-4">
+                <Card className="w-full max-w-[47rem] h-70 !flex !flex-row mx-auto">
+                  {/* Image Section */}
+                  <CardHeader
+                    shadow={false}
+                    floated={false}
+                    className="w-2/5 shrink-0 rounded-r-none"
+                  >
+                    <img
+                      src={event.sponsorimage}
+                      alt="sponsor"
+                      className="h-full w-full object-cover"
+                    />
+                  </CardHeader>
+
+                  {/* Text & Button Section */}
+                  <div className="flex flex-col w-3/5 h-full">
+                    <CardBody className="flex-grow cardsponsor-body">
+                      <Typography
+                        variant="h4"
+                        color="blue-gray"
+                        className="mb-2"
+                      >
+                        {event.sponsornames}
+                      </Typography>
+                      <Typography color="gray" className="mb-2">
+                        {event.sponsordescription}
+                      </Typography>
+                    </CardBody>
+
+                    <CardFooter className="flex justify-end mt-auto cardsponsor-body">
+                      <a href="#" className="inline-block">
+                        <Button
+                          variant="text"
+                          className="flex items-center gap-2"
+                        >
+                          Learn More
+                          <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            fill="none"
+                            viewBox="0 0 24 24"
+                            stroke="currentColor"
+                            strokeWidth={2}
+                            className="h-4 w-4"
+                          >
+                            <path
+                              strokeLinecap="round"
+                              strokeLinejoin="round"
+                              d="M17.25 8.25L21 12m0 0l-3.75 3.75M21 12H3"
+                            />
+                          </svg>
+                        </Button>
+                      </a>
+                    </CardFooter>
+                  </div>
+                </Card>
+              </div>
+            ))}
+          </Slider>
         </div>
       </main>
       <footer>
